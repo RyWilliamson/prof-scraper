f4ca94b6d8d5300dda48e66249774ed7df6dc121
Ryan Williamson <w.rjwilliamson@gmail.com>
2020-11-17 18:18:54 +0000

    Scraping only first list.

--- src/ProfScrapeLib.hs
+++ src/ProfScrapeLib.hs
@@ -4,5 +4,24 @@ module ProfScrapeLib
     (  numProfessors
     ) where
 
+import Text.HTML.Scalpel
+import Data.List
+
 numProfessors :: String -> IO (Maybe Int)
-numProfessors x = return Nothing
+numProfessors [] = return Nothing
+numProfessors school = do
+    names <- scrapeURL (constructUrl school) scrapeList
+    return (countProfessors names)
+
+constructUrl :: String -> String
+constructUrl school = "https://www.gla.ac.uk/schools/" ++ school ++ "/staff/"
+
+scrapeList :: Scraper String [String]
+scrapeList = chroot ("ul" @: ["id" @= "research-teachinglist"]) scrapeNames
+
+scrapeNames :: Scraper String [String]
+scrapeNames = texts "a"
+
+countProfessors :: Maybe [String] -> Maybe Int
+countProfessors Nothing = Nothing
+countProfessors (Just names) = Just (length (filter (\n -> isInfixOf "Professor" n) names))
