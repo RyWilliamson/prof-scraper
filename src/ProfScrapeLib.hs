06be170244d390404c139e747612cbd602dda215
Ryan Williamson <w.rjwilliamson@gmail.com>
2020-11-17 22:07:08 +0000

    Completed Task 1

--- src/ProfScrapeLib.hs
+++ src/ProfScrapeLib.hs
@@ -11,17 +11,21 @@ numProfessors :: String -> IO (Maybe Int)
 numProfessors [] = return Nothing
 numProfessors school = do
     names <- scrapeURL (constructUrl school) scrapeList
-    return (countProfessors names)
+    return (countProfessorsList names)
 
 constructUrl :: String -> String
 constructUrl school = "https://www.gla.ac.uk/schools/" ++ school ++ "/staff/"
 
-scrapeList :: Scraper String [String]
-scrapeList = chroot ("ul" @: ["id" @= "research-teachinglist"]) scrapeNames
+scrapeList :: Scraper String [[String]]
+scrapeList = chroot ("div" @: ["id" @= "tabs"]) scrapeNames
 
-scrapeNames :: Scraper String [String]
-scrapeNames = texts "a"
+scrapeNames :: Scraper String [[String]]
+scrapeNames = chroots ("ul" @: [notP ("id" @= "honorary-visitinglist")]) (texts "a")
 
-countProfessors :: Maybe [String] -> Maybe Int
-countProfessors Nothing = Nothing
-countProfessors (Just names) = Just (length (filter (\n -> isInfixOf "Professor" n) names))
+countProfessors :: [String] -> Int
+countProfessors [] = 0
+countProfessors names = length (filter (\n -> isInfixOf "Professor" n) names)
+
+countProfessorsList :: Maybe [[String]] -> Maybe Int
+countProfessorsList Nothing = Nothing
+countProfessorsList (Just names) = Just (foldl (\x y -> x + (countProfessors y)) 0 names )
