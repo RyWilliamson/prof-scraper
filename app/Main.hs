dc26178e023d6cab5a593e85e20a05c059dab2ce
Ryan Williamson <w.rjwilliamson@gmail.com>
2020-11-18 12:17:16 +0000

    Completed part 2 by outputting a csv

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 module Main where
 
 import Text.HTML.Scalpel
 import ProfScrapeLib
+import Data.Csv
+import GHC.Generics
+import qualified Data.Vector as Vector
+import qualified Data.ByteString.Lazy as ByteString
 
 schools :: [String]
 schools = [
@@ -14,9 +21,20 @@ schools = [
           "psychology"
          ]
 
+data Info = Info { school :: !String, professor :: !(Maybe Int) } deriving (Generic, Show)
+
+instance FromNamedRecord Info
+instance ToNamedRecord Info
+instance DefaultOrdered Info
+
+resultsToRecords :: [(String, Maybe Int)] -> [Info]
+resultsToRecords results = map (\x -> Info (fst x) (snd x)) results
+
 main :: IO ()
 main = do
      counts <- mapM numProfessors schools
      let results = zip schools counts
      mapM_ (\x -> putStrLn $ (fst x) ++ ": " ++ ((show.snd) x)) results
+     let records = resultsToRecords results
+     ByteString.writeFile "output.csv" (encodeDefaultOrderedByName records)
 
